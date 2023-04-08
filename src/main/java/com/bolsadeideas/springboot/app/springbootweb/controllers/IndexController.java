522ac1198d00104af44fdc7d1c15d2ce47bdd3e2
Sapo <benja.v.o@hotmail.com>
2023-04-08 14:42:06 -0600

    Listado de usuarios, uso del decorador ModelAttribute

--- src/main/java/com/bolsadeideas/springboot/app/springbootweb/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/app/springbootweb/controllers/IndexController.java
@@ -1,8 +1,12 @@
 package com.bolsadeideas.springboot.app.springbootweb.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.bolsadeideas.springboot.app.springbootweb.models.Usuario;
@@ -24,10 +28,29 @@ public class IndexController {
         
         usuario.setNombre("Benjamín");
         usuario.setApellido("Osorio");
+        usuario.setEmail("benja.v.o@hotmail.com");
 
         model.addAttribute("titulo", "Perfil de usuario: ".concat(usuario.getNombre()));
         model.addAttribute("usuario", usuario);
         
         return "perfil";
+    }
+
+    @RequestMapping("/listar")
+    public String listar(Model model) {
+        model.addAttribute("titulo", "Listado de usuarios");
+
+        return "listar";
+    }
+
+    @ModelAttribute("usuarios")
+    public List<Usuario> poblarUsuarios() {
+        List<Usuario> usuarios = new ArrayList<>();
+
+        usuarios.add(new Usuario("Benjamín", "Osorio", "benja.v.o@hotmail.com"));
+        usuarios.add(new Usuario("Santiago", "Sueldo", "santiagosueldo@hotmail.com"));
+        usuarios.add(new Usuario("Erwin", "Hernández", "erwinazo@hotmail.com"));
+        
+        return usuarios;
     }
 }
--- src/main/java/com/bolsadeideas/springboot/app/springbootweb/models/Usuario.java
+++ src/main/java/com/bolsadeideas/springboot/app/springbootweb/models/Usuario.java
@@ -3,6 +3,15 @@ package com.bolsadeideas.springboot.app.springbootweb.models;
 public class Usuario {
     private String nombre;
     private String apellido;
+    private String email;
+
+    public Usuario() {}
+
+    public Usuario(String nombre, String apellido, String email) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+    }
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
@@ -12,11 +21,19 @@ public class Usuario {
         this.apellido = apellido;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getNombre() {
         return this.nombre;
     }
 
     public String getApellido() {
         return this.apellido;
+    }
+
+    public String getEmail() {
+        return this.email;
     }
 }
