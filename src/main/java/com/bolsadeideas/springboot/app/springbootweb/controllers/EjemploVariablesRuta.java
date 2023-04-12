400190e6ae3b7535a54cf4e30231df0f481cafe6
Sapo <benja.v.o@hotmail.com>
2023-04-11 18:00:58 -0600

    Avanzando

---
+++ src/main/java/com/bolsadeideas/springboot/app/springbootweb/controllers/EjemploVariablesRuta.java
@@ -0,0 +1,18 @@
+package com.bolsadeideas.springboot.app.springbootweb.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/variables")
+public class EjemploVariablesRuta {
+    @GetMapping("/string/{texto}")
+    public String variables(@PathVariable String texto, Model model) {
+        model.addAttribute("titulo", "Recibir parámetros de la ruta (@PathVariable)");
+        model.addAttribute("resultado", "El texto enviado en la ruta es: " +  texto);
+        return "variables/ver";
+    }
+}
