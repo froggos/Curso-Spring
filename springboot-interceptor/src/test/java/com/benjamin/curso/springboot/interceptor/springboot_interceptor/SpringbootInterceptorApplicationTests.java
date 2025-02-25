16b7fbcb4875bd8cea88f9b48e5a0b71e97d1c57
sapitolindo <benja.v.o@hotmail.com>
2025-02-25 14:01:59 -0300

    Seccion 6 terminada

---
+++ springboot-interceptor/src/test/java/com/benjamin/curso/springboot/interceptor/springboot_interceptor/SpringbootInterceptorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benjamin.curso.springboot.interceptor.springboot_interceptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootInterceptorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
